cc4ddb1e583702bc2e0652041aec2b697280245b
Adi Eyal <adi@burgercom.co.za>
2011-09-23 13:06:42 +0200

    Added application for download

--- server/tendai/urls.py
+++ server/tendai/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -8,9 +10,11 @@ from django.http import HttpResponseRedirect
 from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('',
-    url(r'^$', redirect_to, {'url' :'/admin'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^openrosa/', include('openrosa.urls')),
+    ('^$', direct_to_template, {
+        'template': 'home.html'
+    })
 )
 
 if settings.DEBUG:
@@ -18,3 +22,4 @@ if settings.DEBUG:
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT,}),
     )
 
+    urlpatterns += staticfiles_urlpatterns()
