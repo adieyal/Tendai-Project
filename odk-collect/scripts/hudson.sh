74e34e0ac0e31edc18a0275647bbdfd28cdc350f
Adi Eyal <adi@sarpam.net>
2011-11-24 03:40:22 -0600

    No longer need odk-collect

--- odk-collect/scripts/hudson.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-export WORKSPACE=`pwd`
-export PATH=$WORKSPACE/.env/bin:$PATH
-
-cd bksmobi
-ant debug
-
--- odk-collect/src/org/odk/collect/android/listeners/AdvanceToNextListener.java
+++
@@ -1,6 +0,0 @@
-package org.odk.collect.android.listeners;
-
-public interface AdvanceToNextListener {
-
-        void next(); //Move on to the next question
-}
