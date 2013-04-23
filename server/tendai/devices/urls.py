8ed37401c2a6ec4fc031ab8b19dcdf21ac955276
Adi Eyal <adi@burgercom.co.za>
2013-04-23 09:13:19 +0200

    Removed old submissions_overview view
    No longer needed

--- server/tendai/devices/urls.py
+++ server/tendai/devices/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     (r'^formXml/(?P<country_code>[a-z]{2})/$', "devices.views.formXml", {}, "openrosa_dynamic_formxml"),
     (r'^formXml/(?P<country_code>[a-z]{2})/(?P<language>[a-z]{2})/$', "devices.views.formXml", {}, "openrosa_dynamic_formxml"),
     (r'^swd/(?P<id>\d+)/$', "devices.views.view_swd", {}, "devices_view_swd"),
-    (r'^submissions/$', "devices.views.view_submissions", {}, "devices_view_submissions"),
 )
 
 urlpatterns += openrosa_urls.urlpatterns
