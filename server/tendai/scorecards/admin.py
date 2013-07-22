7e08824e329b3ff508d0312b844f6e7ef56d0780
Adi Eyal <adi@burgercom.co.za>
2013-07-22 14:34:47 +0200

    Added admin fields to scorecard story

--- server/tendai/scorecards/admin.py
+++ server/tendai/scorecards/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 import models
 
 class ScorecardStoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('community_worker', 'created_date', 'country') 
 
+    def community_worker(self, obj):
+        return obj.submission_worker_device.community_worker
+
+    def created_date(self, obj):
+        return obj.submission_worker_device.created_date
+
+    def country(self, obj):
+        return obj.submission_worker_device.community_worker.country
 admin.site.register(models.ScorecardStory, ScorecardStoryAdmin)
